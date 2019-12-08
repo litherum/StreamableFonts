18854cb43e5dee394bcc794f42c3f8a404f49e08
Litherum <litherum@gmail.com>
2019-12-07 19:23:08 -0800

    Removing previous genetic algorithm

--- v2/Optimize/GeneticAlgorithm/main.m
+++ v2/Optimize/GeneticAlgorithm/main.m
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[]) {
         Seeds *seeds = [Seeds new];
         GeneticAlgorithm *geneticAlgorithm = [[GeneticAlgorithm alloc] initWithGlyphData:glyphData andSeeds:seeds.seeds];
         NSDate *start = [NSDate date];
-        [geneticAlgorithm runIterations:10 withCallback:^void (void) {
+        [geneticAlgorithm runIterations:1 withCallback:^void (void) {
             NSDate *end = [NSDate date];
             NSLog(@"Complete. %f ms", [end timeIntervalSinceDate:start] * 1000);
             CFRunLoopStop(CFRunLoopGetMain());
