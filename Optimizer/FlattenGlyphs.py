f2c21cd49dbb8e64b00460cd8ef91e37a99c8994
Myles C. Maxfield <mmaxfield@apple.com>
2020-07-03 00:24:33 -0700

    Adding CFF support to FlattenGlyphs

--- Optimizer/FlattenGlyphs.py
+++ Optimizer/FlattenGlyphs.py
@@ -1,8 +1,41 @@
 from fontTools.ttLib.ttFont import TTFont
 from fontTools.ttLib.tables import ttProgram
+from fontTools.misc import psCharStrings
 
+class FlattenError(Exception):
+  """We couldn't figure out the range requests to send."""
+
+def inlineProgram(localSubrs, globalSubrs, program):
+    if len(program) < 2:
+        return program
+
+    inlinedProgram = []
+    context = program[0]
+    for i in range(1, len(program)):
+        if program[i] == "callsubr":
+            index = context + psCharStrings.calcSubrBias(localSubrs)
+            inlinedProgram.extend(inlineProgram(localSubrs, globalSubrs, localSubrs[index].program))
+            context = None
+        elif program[i] == "callgsubr":
+            index = context + psCharStrings.calcSubrBias(globalSubrs)
+            inlinedProgram.extend(inlineProgram(localSubrs, globalSubrs, globalSubrs[index].program))
+            context = None
+        elif program[i] == "return":
+            if context is not None:
+                inlinedProgram.append(context)
+            return inlinedProgram
+        else:
+            if context is not None:
+                inlinedProgram.append(context)
+            context = program[i]
+    if context is not None:
+        inlinedProgram.append(context)
+    return inlinedProgram
+
+  
 def flattenGlyphs(input, fontNumber, output):
     font = TTFont(input, fontNumber=fontNumber)
+    font.recalcBBoxes = False
     if "glyf" in font:
         for glyphName in font.getGlyphOrder():
             glyph = font["glyf"][glyphName]
@@ -13,5 +46,18 @@ def flattenGlyphs(input, fontNumber, output):
             glyph.flags = flags
             glyph.program = ttProgram.Program()
             font["glyf"][glyphName] = glyph
+    elif "CFF " in font:
+        cff = font["CFF "]
+        fontName = cff.cff.fontNames[0]
+        topDict = cff.cff[fontName]
+        for glyphID in range(len(font.getGlyphOrder())):
+            charString = topDict.CharStrings.charStringsIndex[glyphID]
+            charString.decompile()
+            localSubrs = getattr(charString.private, "Subrs", [])
+            globalSubrs = charString.globalSubrs
+            inlinedProgram = inlineProgram(localSubrs, globalSubrs, charString.program)
+            charString.program = inlinedProgram
+    else:
+        raise FlattenError("Could not flatten glyphs.")
 
     font.save(output)
