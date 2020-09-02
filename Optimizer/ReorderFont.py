7c78f3cebfa79165a962dfc56962538a1aa1fede
Garret Rieger <grieger@google.com>
2020-09-02 14:21:31 -0700

    Fix re-ordering of glyphs in the optimized font. (#1)

--- Optimizer/ReorderFont.py
+++ Optimizer/ReorderFont.py
@@ -39,8 +39,48 @@ def reorderGlyphs(glyphOrder, desiredGlyphOrder):
         result.append(glyphOrder[desiredGlyph])
     return result
 
+def fixLayoutCoverage(font):
+    # Coverage sub tables inside of ot layout tables (GPOS, GSUB)
+    # must have glyphs sorted by numeric order. FontTools does
+    # not correct ordering problems on compilation, so we must
+    # do it ourselves.
+    if "GPOS" in font:
+        dfs(font["GPOS"], font)
+    if "GSUB" in font:
+        dfs(font["GSUB"], font)
+
+def dfs(table, font):
+    if type(table).__name__ == "Coverage":
+        fixGlyphOrder(table.glyphs, font)
+        return
+
+    if type(table) == list:
+      values = table
+    elif type(table) == dict:
+      values = table.values()
+    elif not table or type(table) == int or type(table) == str:
+      values = []
+    else:
+      values = table.__dict__.values()
+
+    for sub in values:
+        dfs(sub, font)
+
+def fixGlyphOrder(glyphs, font):
+    glyph_ids = sorted([font.getGlyphID(gname) for gname in glyphs])
+    del glyphs[:]
+    glyphs.extend([font.getGlyphOrder()[gid] for gid in glyph_ids])
+
+def fullyLoadFont(font):
+    # Run the save to xml which will touch all tables and sub tables
+    # forcing full decompilation of the original font.
+    tmp = BytesIO()
+    font.saveXML(tmp)
+    return font
+
 def reorderFont(input, fontNumber, desiredGlyphOrder, output):
-    font = TTFont(input, fontNumber=fontNumber)
+    font = TTFont(input, fontNumber=fontNumber, lazy=False)
+    font = fullyLoadFont(font)
 
     if "CFF " in font:
         cff = font["CFF "]
@@ -52,9 +92,18 @@ def reorderFont(input, fontNumber, desiredGlyphOrder, output):
     reorderedGlyphs = reorderGlyphs(glyphOrder, desiredGlyphOrder)
     if reorderedGlyphs is None:
         return False
+
     font.setGlyphOrder(reorderedGlyphs)
+    # Glyph order is cached in a few places, clear those out.
+    del font["glyf"].glyphOrder
+    if hasattr(font, "_reverseGlyphOrderDict"):
+      del font._reverseGlyphOrderDict
+
+    fixLayoutCoverage(font)
+
     tmp = BytesIO()
     font.save(tmp)
+
     tableOrder = font.reader.keys()
     success = computeTableOrder(tableOrder)
     if not success:
@@ -64,4 +113,5 @@ def reorderFont(input, fontNumber, desiredGlyphOrder, output):
     reorderFontTables(tmp, outputStream, tableOrder)
     tmp.close()
     outputStream.close()
+
     return True
